c1fbe87f735b3ea9598b8f82709d198314db5e7d
adarsht00001 <adarsht00001@gmail.com>
2023-03-03 10:36:27 +0530

    eslint setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import  ReactDOM  from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-const root=ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<>'Hello'</>)
+root.render(<>Hello</>);
