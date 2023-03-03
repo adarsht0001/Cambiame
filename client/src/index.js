d6506708c9eb66f3a92861b9bfcd39f98698d4ae
adarsht00001 <adarsht00001@gmail.com>
2023-03-03 13:12:39 +0530

    initial commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import BasicTextFields from './Components/InputField';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<>Hello</>);
+root.render(<BasicTextFields />);
