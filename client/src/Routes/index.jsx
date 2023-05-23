d80ec00943e9bce386bdffcbc560cae3c09bdb52
adarsht0001 <adarsht00001@gmail.com>
2023-05-23 14:35:57 +0530

    [update] error page added

--- client/src/Routes/index.jsx
+++ client/src/Routes/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import UserRoutes from './UserRoutes';
 import AdminRoutes from './AdminRoutes';
+import Error from '../Pages/eroor/Error';
 
 function Router() {
   return (
     <BrowserRouter>
       <UserRoutes />
       <AdminRoutes />
+      <Routes>
+        <Route path="*" element={<Error />} />
+      </Routes>
     </BrowserRouter>
   );
 }
