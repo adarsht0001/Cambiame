a8a69cf541babccd5638a49cca9b95f3fac310c1
adarsht0001 <adarsht00001@gmail.com>
2023-05-24 20:51:55 +0530

    [fix] error page fixed

--- client/src/Routes/index.jsx
+++ client/src/Routes/index.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import UserRoutes from './UserRoutes';
 import AdminRoutes from './AdminRoutes';
-import Error from '../Pages/eroor/Error';
+// import Error from '../Pages/error/Error';
 
 function Router() {
   return (
     <BrowserRouter>
       <UserRoutes />
       <AdminRoutes />
-      <Routes>
-        <Route path="*" element={<Error />} />
-      </Routes>
+      {/* <Routes>
+        <Route path="*" exact element={<Error />} />
+      </Routes> */}
     </BrowserRouter>
   );
 }
