be40ee288a5c98d8497299718e2a7873ecb2f38e
adarsht0001 <adarsht00001@gmail.com>
2023-05-29 03:57:32 +0530

    workflow fix

--- client/src/Axios/axios.js
+++ client/src/Axios/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const baseURL = '/api';
-const baseURL = 'http://localhost:5000/api';
+const baseURL = '/api';
+// const baseURL = 'http://localhost:5000/api';
 
 const instance = axios.create({
   baseURL,
