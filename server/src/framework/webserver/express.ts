37c04c45b18af0c7b51b99ed8d3f2e3b79a4aa73
adarsht0001 <adarsht00001@gmail.com>
2023-06-18 21:03:43 +0530

    set up

--- server/src/framework/webserver/express.ts
+++ server/src/framework/webserver/express.ts
@@ -6,17 +6,13 @@ import helmet from "helmet";
 import mongoSanitize from "express-mongo-sanitize";
 
 const expressConfig = (app: Application) => {
-  // Development logging
-  // if (configKeys.nodeEnv == 'development') {
   app.use(morgan("dev"));
-  // }
 
   // app.use(cors({ origin: "http://localhost:3000" }));
   app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   // app.use(cookieParser());
-  // app.use(helmet({xssFilter:true}))
 
   app.use(mongoSanitize());
 };
